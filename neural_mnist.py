5c5d8b54f5735339459be4405685675e8b94cacc
Lene Preuss <lene.preuss@here.com>
2017-10-11 17:20:41 +0200

    - made fancier MNIST models work - added visualisation of the model

--- neural_mnist.py
+++ neural_mnist.py
@@ -1,25 +1,27 @@
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense, Activation, Flatten
 
 from data_sets.mnist_data_sets import MNISTDataSets
 
 
 def very_simple_mnist():
     return Sequential([
-        Dense(32, input_dim=data.num_features), Activation('relu'),
-        Dense(data.num_labels), Activation('softmax'),
+        Flatten(input_shape=(28, 28, 1)),
+        Dense(32, activation='relu'),
+        Dense(data.num_labels, activation='softmax')
     ])
 
 
 def simple_mnist():
     return Sequential([
-        Dense(data.num_features, input_dim=data.num_features), Activation('relu'),
-        Dense(data.num_labels), Activation('softmax'),
+        Flatten(input_shape=(28, 28, 1)),
+        Dense(data.num_features, input_dim=data.num_features, activation='relu'),
+        Dense(data.num_labels, activation='softmax'),
     ])
 
 
 def fancy_mnist():
-    from keras.layers import Dropout, Flatten, Conv2D, MaxPooling2D
+    from keras.layers import Dropout, Conv2D, MaxPooling2D
     # https://github.com/fchollet/keras/blob/master/examples/mnist_cnn.py
     return Sequential([
         Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)),
@@ -34,20 +36,21 @@ def fancy_mnist():
 
 
 def even_fancier_mnist():
-    from keras.layers import Dropout, Flatten, Conv2D, MaxPooling2D
+    from keras.layers import Dropout, Conv2D, MaxPooling2D
     # http://machinelearningmastery.com/handwritten-digit-recognition-using-convolutional-neural-networks-python-keras/
-    model = Sequential()
-    model.add(Conv2D(30, 5, 5, border_mode='valid', input_shape=(1, 28, 28), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(15, 3, 3, activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.2))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dense(50, activation='relu'))
-    model.add(Dense(10, activation='softmax'))
+    model = Sequential([
+        Conv2D(30, (5, 5), padding='valid', input_shape=(28, 28, 1), activation='relu'),
+        MaxPooling2D(pool_size=(2, 2)),
+        Conv2D(15, (3, 3), activation='relu'),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dropout(0.2),
+        Flatten(),
+        Dense(128, activation='relu'),
+        Dense(50, activation='relu'),
+        Dense(data.num_labels, activation='softmax')
+    ])
     # Compile model
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
 
@@ -76,23 +79,37 @@ def nth_index_and_value(l, n):
 data = MNISTDataSets('./data', True)
 
 mnist = very_simple_mnist()
+# mnist = simple_mnist()
+# mnist = fancy_mnist()
+# mnist = even_fancier_mnist()
 
 mnist.compile(
     optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy']
 )
 
-mnist.fit(data.train.input, data.train.labels, epochs=10)
+# simple models:
+# mnist.fit(data.train.input, data.train.labels, epochs=10)
+# loss_and_metrics = mnist.evaluate(data.test.input, data.test.labels)
+# fancy models:
+mnist.fit(data.train.input.reshape(55000, 28, 28, 1), data.train.labels, epochs=1)
+loss_and_metrics = mnist.evaluate(data.test.input.reshape(10000, 28, 28, 1), data.test.labels)
 
-loss_and_metrics = mnist.evaluate(data.test.input, data.test.labels)
 print()
 print('test set loss:', loss_and_metrics[0], 'test set accuracy:', loss_and_metrics[1])
+
+from keras.utils import plot_model
+from subprocess import call
+
+plot_model(mnist, show_shapes=True, to_file='model.png')
+call(['xv', 'model.png'])
 
 
 for i in range(0, 10):
     actual = data.test.labels[i]
-    print('actual:', maxindex(actual), actual)
+    print('actual:   ', maxindex(actual), actual)
 
     show_image(data.test.input[i])
 
-    prediction = mnist.predict(data.test.input[i:i + 1])[0]
+    # prediction = mnist.predict(data.test.input[i:i + 1])[0]
+    prediction = mnist.predict(data.test.input[i:i + 1].reshape(1, 28, 28, 1))[0]
     print('predicted:', maxindex(prediction), nth_index_and_value(prediction, 1), 'runner up:', nth_index_and_value(prediction, 2))
